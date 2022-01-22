f9f646637fe66e5b59491c42ad15b87e36d8e6c5
amda <amda.prog@gmail.com>
2022-01-22 14:58:48 +0100

    Aliased modules

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 console.log("Hello World!");
 
-// TODO: Aliased modules
 // TODO: Introduce the following data using the command line
+// TODO: Validate input, set default inputs
 // TODO: Select the way to introduce the data (JSON or Manual)
 // TODO: Compute the maximum height of the projectile
 // TODO: Compute the maximum traveled distance
