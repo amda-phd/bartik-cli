f5a1a9366a9172f97333cb4f0349d38bf8773bd3
amda <amda.prog@gmail.com>
2022-01-22 19:35:13 +0100

    Design CLI data input

--- bin/index.js
+++ bin/index.js
@@ -5,20 +5,34 @@ const { program } = require("commander");
 const chalk = require("chalk");
 const { log } = console;
 
-const { version, description, name } = require("@pack");
+const { version, description } = require("@pack");
+
+const DEFAULT_INPUT = "initialValues.json";
+const DEFAULT_OUTPUT = "results.json;";
 
 program
-  .name(name)
   .description(description)
   .version(version)
-  .option("-v0, --initialVelocity <velocity>")
+  .option(
+    "-m, --manual <initialVelocity, alpha>",
+    "Introduce initial velocity and inclination manually, separated by a comma"
+  )
+  .option(
+    "-j, --JSON [inputFileName]",
+    `Compute ballistics for the values contained in a JSON file. If no filename is provided, the program will look for ./${DEFAULT_INPUT}`,
+    DEFAULT_INPUT
+  )
+  .option(
+    "-o, --output [outputFileName]",
+    `Name of the file where the results will be stored. If the option is not invoqued, the results will only be presented through the logshell. If no output file name is provided, the results will be saved on ${DEFAULT_OUTPUT}`,
+    DEFAULT_OUTPUT
+  )
   .parse();
 
 log(chalk.blue("Hello World!"));
 const options = program.opts();
 if (options.initialVelocity) console.log(options.initialVelocity);
 
-// TODO: Introduce the following data using the command line
 // TODO: Validate input, set default inputs
 // TODO: Select the way to introduce the data (JSON or Manual)
 // TODO: Compute the maximum height of the projectile
