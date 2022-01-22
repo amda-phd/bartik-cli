33ed923eeba690cabd03ce81446b142fb8d0d551
amda <amda.prog@gmail.com>
2022-01-22 11:38:00 +0100

    Add tasks as TODOs

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
 console.log("Hello World!");
+
+// TODO: Introduce the following data using the command line
+// TODO: Select the way to introduce the data (JSON or Manual)
+// TODO: Compute the maximum height of the projectile
+// TODO: Compute the maximum traveled distance
+// TODO: Save the computed data (Inputs + Results) into a file
