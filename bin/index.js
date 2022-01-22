d87da17204ae825a7db26fbce6ce10cb38097418
amda <amda.prog@gmail.com>
2022-01-22 18:30:13 +0100

    Add some color with chalk

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,8 @@
 
 require("module-alias/register");
 const { program } = require("commander");
+const chalk = require("chalk");
+const { log } = console;
 
 const { version, description, name } = require("@pack");
 
@@ -12,7 +14,7 @@ program
   .option("-v0, --initialVelocity <velocity>")
   .parse();
 
-console.log("Hello World!");
+log(chalk.blue("Hello World!"));
 const options = program.opts();
 if (options.initialVelocity) console.log(options.initialVelocity);
 
